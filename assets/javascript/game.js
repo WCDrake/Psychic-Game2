e98bbc39a008f353c7d9decdb9585c6aea8998d1
unknown <wconnordrake@gmail.com>
2018-05-08 18:25:08 -0400

    final commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,53 +2,46 @@
 
 var letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var choose = Math.floor(Math.random() * 26);
-var computerChoice = letter [choose];
+var computerChoice = letter[choose];
 var win = 0;
-var guesses= 9;
-var loss= 0;
+var guesses = 9;
+var loss = 0;
 var guessesSoFar = [];
 
-
-
-
-
-
-console.log (computerChoice);
+console.log(computerChoice); 
 
 document.onkeyup = function (event) {
-    guessesSoFar.push(letter);
-    var letter = event.key.toLowerCase();
+    var userChoice = event.key.toLowerCase();
 
-     if (letter === computerChoice){
 
-        win= win + 1;
+
+    if (userChoice === computerChoice) {
+
+        win = win + 1;
         document.getElementById("winCount").innerHTML = "Wins: " + win;
-        guesses= 9;
+        guesses = 9;
         guessesSoFar = [];
-        
-    
-        } else if (guesses <1){
-            loss= loss + 1;
-            document.getElementById("lossCount").innerHTML = "Losses: " + loss;
-            guesses= 9;
-            guessesSoFar = [];
-        }
+        var newChoice = Math.floor(Math.random() * 26);
+        computerChoice = letter[newChoice];
 
-      else if (letter != computerChoice) {
-         guesses= guesses - 1;
-        guessesSoFar.push(letter);
-         document.getElementById("guessCount").innerHTML = "Guesses: " + guesses;
-         document.getElementById("lettersLog").innerHTML = "Letters Guessed: " + guessesSoFar;
+    } else if (guesses < 1) {
+        loss = loss + 1;
+        document.getElementById("lossCount").innerHTML = "Losses: " + loss;
+        guesses = 9;
+        guessesSoFar = [];
+    }
 
-    
-     }
+    else if (userChoice != computerChoice) {
+        guesses = guesses - 1;
+        guessesSoFar.push(userChoice);
+        document.getElementById("guessCount").innerHTML = "Guesses Left: " + guesses;
+        document.getElementById("lettersLog").innerHTML = "Your Guesses so far: " + guessesSoFar;
+
+
+    }
 };
-        
 
-/*1) I need a way to fix the guesses so far (commas show up)
-  2) I need a way to clear the letter log with a win. and the guess count. 
-  3) I need a way for the variable computerChoice to refresh
-*/
-     
+
+
 
 
